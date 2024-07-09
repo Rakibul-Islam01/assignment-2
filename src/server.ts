45cb34777f74fa2a463702e83f18c37870415c7c
Rakibul-Islam01 <iamrakibulislam01@gmail.com>
2024-07-09 11:34:52 +0600

    eslint installed

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,6 @@ async function main() {
     try {
         await mongoose.connect(config.data_url as string);
 
-    const port = 5000;
     app.listen(config.port, () => {
         console.log(`Example app listening on port ${config.port}`)
       })
